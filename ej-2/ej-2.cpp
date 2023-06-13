415a97ce5380f846f30e83147af1d3450fcde3b7
Pablo Zaid <pablozaid2002@gmail.com>
2023-06-13 15:25:48 -0300

    Se agregan optimizaciones de tiempo y más comentarios

--- ej-2/ej-2.cpp
+++ ej-2/ej-2.cpp
@@ -1,16 +1,25 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> N; // Red de flujo. La usamos para iterar por todas las aristas en O(m)
-vector<vector<int>> capacitado; //matriz de capacidades. En una posición ij el valor  es
+//El problema se puede pensar como uno de corte mínimo:
+//Al conectar los que prefieren Prim a la fuente, y los de Kruskal al sumidero, minimizar la discomformidad
+//es equivalente a buscar un corte mínimo: dos amigos en S y V\S generan uno de discomformidad porque de S
+//sale una arista de capacidad uno. La discomformidad de cambiarse de bando se ve en la relación que
+//tienen los vértices con la fuente y el sumidero: si un estudiante votaba a Prim, pero se pasa a Kruskal,
+// hay una nueva arista de S a V\S (de la fuente a ese estudiante) por lo que aumenta en uno.
+//Si votaba a Kruskal también ocurre, pues ahora la arista es de el estudiante al sumidero.
+//Para hacer la solución tomamos un algoritmo de flujo máximo, ya que son equivalentes
+
+vector<vector<int>> N; // Red de flujo. La usamos para iterar por todas las aristas en O(m) en vez de O(n^2)
+vector<vector<int>> capacitado; //matriz de flujos. En una posición ij el valor  es
                                 // - 1 si la arista ij no existe, 0 si no tiene flujo
                                 // y 1 si lo tiene
 vector<vector<int>> R;          // red residual
-vector<bool> marcados;          // vector de booleanos para el dfs
 
-bool BFS_aumento(vector<int>&CDA) {
-    CDA[0] = -2;
+bool BFS_aumento(vector<int>&padre) {
+    padre[0] = -2;
     queue<int> q;
     q.push(0);
 
@@ -19,9 +28,9 @@ bool BFS_aumento(vector<int>&CDA) {
         q.pop();
 
         for (int next : N[cur]) {
-            if (CDA[next] == -1 && !capacitado[cur][next]) {
-                CDA[next] = cur;
-                if (next == CDA.size() - 1) return true;
+            if (padre[next] == -1 && !capacitado[cur][next]) {
+                padre[next] = cur;
+                if (next == padre.size() - 1) return true; //encontró a t, se termina el camino de aumento
                 q.push(next);
             }
         }
@@ -30,57 +39,12 @@ bool BFS_aumento(vector<int>&CDA) {
     return false;
 }
 
-void DFS_aumento(vector<int>&CDA, int v) {
-    marcados[v] = true;
-    if (v == N.size()+1) return;
-    for (int w : R[v]) {
-        if (marcados[w]) continue;
-        else {
-            CDA[w] = v; //marcamos los anteriores para reconstruir el camino aumentado
-            DFS_aumento(CDA,w);
-        }
-    }
-}
-
-void armar_camino(vector<int>&CDA) {
-    vector<int> temp;
-    int i = CDA.size()-1;
-    while(i != -1) {
-        temp.push_back(i);
-        i = CDA[i];
-    }
-    CDA = temp;
-}
-
 int corte_minimo() {
+    //Implementación de https://cp-algorithms.com/graph/edmonds_karp.html#implementation
     int discomformidad = 0;
     vector<int> caminoDeAumento;
     caminoDeAumento.resize(N.size(), -1);
     while (BFS_aumento(caminoDeAumento)) {
-        //R = {{}};
-        //R.resize(N.size(), {});
-        /*for (int i = 0; i < N.size(); i++) {    //Armamos la red de aumento de acuerdo a su definicion
-            for (int j = 0; j < N[i].size(); j++) {
-                if (!capacitado[i][N[i][j]]) R[i].push_back(N[i][j]);
-                if (capacitado[i][N[i][j]]) R[N[i][j]].push_back(i);
-            }
-        }
-        DFS_aumento(caminoDeAumento, 0);
-        if (!marcados[marcados.size()-1]) { // si está marcado, devolvemos el flujo contando cada unidad
-            int res = 0;                    // de flujo hacia el sumidero
-            for (int i = 0; i < capacitado.size(); i++) {
-                res += (capacitado[i][capacitado.size()-1] == 1);
-            }
-            return res;
-        }
-
-        armar_camino(caminoDeAumento);
-        for (int i = 0; i < caminoDeAumento.size() - 1; i++) {
-            int origen = caminoDeAumento[i + 1]; //Actualizamos los valores de flujo de acuerdo al camino
-            int dest = caminoDeAumento[i];      // de aumento
-            if (capacitado[origen][dest] == 0) capacitado[origen][dest] = 1;
-            if (capacitado[dest][origen] != -1) capacitado[dest][origen] = 0;
-        } */
     discomformidad++;
     int cur = caminoDeAumento.size()-1;
     while (cur != 0) {
@@ -96,22 +60,26 @@ int corte_minimo() {
 }
 
 int main() {
+    // Implementación de https://www.codingninjas.com/codestudio/library/fast-i-o-in-c
+    // Sin esta optimización de IO, el juez considera que el algoritmo es demasiado lento
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n; int m;
     cin >> n >> m;
     while (n != 0 || m != 0) {
         N = {{}}; capacitado = {{}};
         N.resize(n + 2, {}); //Agregamos 2 lugares para la fuente y el sumidero
         vector<vector<int>> capacitador(n+2, vector<int>(n+2,-1));
-        capacitado = capacitador;
+        capacitado = capacitador; //sobreescibimos el vector para cada instancia de m y n
         int k;
         for(int i = 1; i <= n; i++) {//creamos el grafo a partir de las aristas
             cin >> k;                // todas comienzan con flujo 0
             if (k) {
-                N[0].push_back(i);
+                N[0].push_back(i); //Si cree que prim es mejor, lo conectamos a la fuente
                 capacitado[0][i] = 0;
             }
             else {
-                N[i].push_back(n+1);
+                N[i].push_back(n+1); //Si cree que kruskal es mejor, va al sumidero
                 capacitado[i][n+1] = 0;
             }
         }
